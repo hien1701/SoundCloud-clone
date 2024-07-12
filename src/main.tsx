13bc4ad49bfc51f24e91912399be20f390d050be
HienDinh <dinhconghien1701@MacBook-Pro-cua-Dinh.local>
2024-07-12 08:29:46 +0700

    Design UI for header

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,74 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 // import './index.css'
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  Outlet,
+  RouterProvider,
+  Link,
+} from "react-router-dom";
 import UsersPage from "./screens/users.page.tsx";
+import { TeamOutlined, FireOutlined } from "@ant-design/icons";
+import type { MenuProps } from "antd";
+import { Menu } from "antd";
+type MenuItem = Required<MenuProps>["items"][number];
+
+const items: MenuItem[] = [
+  {
+    label: <Link to={"/"}>Home</Link>,
+    key: "home",
+    icon: <FireOutlined />,
+  },
+  {
+    label: <Link to={"/users"}>Manage Users</Link>,
+    key: "users",
+    icon: <TeamOutlined />,
+    // disabled: true,
+  },
+];
+
+const Header: React.FC = () => {
+  const [current, setCurrent] = useState("home");
+
+  const onClick: MenuProps["onClick"] = (e) => {
+    console.log("click ", e);
+    setCurrent(e.key);
+  };
+
+  return (
+    <Menu
+      onClick={onClick}
+      selectedKeys={[current]}
+      mode="horizontal"
+      items={items}
+    />
+  );
+};
+
+const LayoutAdmin = () => {
+  return (
+    <div>
+      <Header />
+      <Outlet />
+      <footer>Footer</footer>
+    </div>
+  );
+};
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App />,
+    element: <LayoutAdmin />,
+    children: [
+      { index: true, element: <App /> },
+      {
+        path: "users",
+        element: <UsersPage />,
+      },
+    ],
   },
-  {
-    path: "/users",
-    element: <UsersPage />,
-  },
+
   {
     path: "/tracks",
     element: <div>Manage Tracks</div>,
